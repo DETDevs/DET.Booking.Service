2c1bcc6926d378d94bade87411b128377fbd97e5
douglas-i <hdouglasu@gmail.com>
2025-05-20 05:35:51 -0600

    feat: add base models

---
+++ DET.Booking.Models/Services.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DET.Booking.Models
+{
+    public class Services
+    {
+        public long ServiceID { get; set; }
+        public string BusinessID { get; set; }
+        public string EmployeeID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string DutarionMin { get; set; }
+        public string Price { get; set; }
+        public string DiasHabiles { get; set; }
+        public bool IsActive { get; set; }
+        public string CreateUser { get; set; }
+        public string CreateDate { get; set; }
+        public string ModificationUser { get; set; }
+        public string ModificationDate { get; set; }
+    }
+}
